2e06f33fb851c4c6d7f362bbb1db52a4ead0aa6f
dheerajpoonia29 <2017pietcsdheeraj035@poornima.org>
2020-08-21 18:18:54 +0530

    21-8-20

--- project/testback/index.js
+++ project/testback/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
 
-app.get('/', (req, res) => {
-    return res.send("hi my code is running on server");
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  return res.send("Home page");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("You are visiting login route");
+});
+
+app.get('/logout', (req, res)=>{
+    return res.send("you are logout");
+})
+
+app.get('/dheeraj', (req, res)=>{
+    return res.send("hi man thanks for checking my routes");
 })
 
 app.listen(port, () => {
-    console.log(`server at ${port} running ...`)
-})
+  console.log("Server is up and running...");
+});
